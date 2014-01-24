c9b661ff078c75bb159e5aee34ee069b0540b79b
Tristan <sinthetyc@gmail.com>
2014-01-24 15:14:09 +0000

    Travel Blog

---
+++ travel/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+		<title>Travel Blog - Coming Soon</title>
+		<link href="style.css" rel="stylesheet" />
+	</head>
+	<body>
+		<section id="splash">
+			<h1>Tristan &amp; Ruth&apos;s Travel Blog</h1>
+			<p>Travel blog coming soon. Featuring India, Thailand, Japan and Malaysia.</p>
+		</section>
+	</body>
+</html>
